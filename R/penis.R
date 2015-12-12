aaff3a2940af6cf946d2bfa0e9a2f42e24978d30
jemus42 <jemus42@quantenbrot.de>
2015-12-12 10:39:35 +0100

    Improve penis documentation

--- R/penis.R
+++ R/penis.R
@@ -4,6 +4,18 @@
 #' @docType data
 #' @keywords dataset
 #' @usage penis
-#' @format A \code{data.frame} containing numerical and factor data.
-#' @source The horribly coded but very informative \url{http://www.everyoneweb.com/worldpenissize}
+#' @format A 146 x 10 \code{data.frame} containing numerical and factor data with the following variables:
+#' \describe{
+#' \item{country}{Country of measurement}
+#' \item{region}{Region of measurement}
+#' \item{length_flaccid}{Flaccid length in cm}
+#' \item{length_erect}{Erect length in cm}
+#' \item{circumf_flaccif}{Flaccid circumference in cm}
+#' \item{circumf_erect}{Erect circumference in cm}
+#' \item{volume_erect}{Erect volum in cm^3}
+#' \item{volume_flaccid}{Flaffid volume in cm^3}
+#' \item{method}{Method of measurement: Self reported & Measured}
+#' \item{n}{Sample size of measurement}
+#' }
+#' @source \url{http://www.everyoneweb.com/worldpenissize}
 "penis"
